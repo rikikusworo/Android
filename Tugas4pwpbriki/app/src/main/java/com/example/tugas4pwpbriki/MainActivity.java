ac55befe2adc15c156bbc71b182ead14a6b718ef
Riki Kusworo <54160293+rikikusworo@users.noreply.github.com>
2019-08-26 15:01:56 +0700

    Tugas4RikiPwpb
    SamsungS10 PWPB 4

---
+++ Tugas4pwpbriki/app/src/main/java/com/example/tugas4pwpbriki/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas4pwpbriki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
